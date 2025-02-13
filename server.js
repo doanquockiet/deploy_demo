bb09871677aa6bf4c7ed8c69582bf8f37158f79f
Doan Quoc Kiet <116879984+doanquockiet@users.noreply.github.com>
2025-02-13 13:01:55 +0700

    update cors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 mongoose
   .connect(process.env.MONGO_URI)
